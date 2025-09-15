3bdf651b3875635547df121c509f348b0f715ce4
Sternee <sternee2332006@gmail.com>
2025-09-15 10:28:18 +0530

    production commit #1

--- rcnb/rcnb/settings.py
+++ rcnb/rcnb/settings.py
@@ -24,12 +24,14 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mevf@t=smu$$(5=%qxmy0f-4cr25x^@42+-+pp%__feaje7rlh'
+# rcnb/rcnb/settings.py
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+# It's also good practice to ensure DEBUG is read from an environment variable
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,15 +89,10 @@ WSGI_APPLICATION = 'rcnb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'RAISE-DB',
-        'USER': 'postgres',
-        'PASSWORD': 'ANON77',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
