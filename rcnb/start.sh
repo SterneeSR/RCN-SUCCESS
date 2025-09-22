8a497708f7da0113e5f7c28bf966173580dd2f31
Sternee <sternee2332006@gmail.com>
2025-09-22 15:10:37 +0530

    SUCCESS COMMIT #9

--- rcnb/start.sh
+++ rcnb/start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
-# Create migrations (as you requested)
-python manage.py makemigrations
-
-# Apply database migrations
 python manage.py migrate
+python manage.py create_superuser_with_password
 
-# Create the superuser if it doesn't exist
-python manage.py createsuperuser
-
-# Start the Gunicorn server which runs your Django app
 gunicorn rcnb.wsgi:application --workers 3 --threads 2 --timeout 60 --log-file -
--- rcnb/users/management/commands/create_superuser_with_password.py
+++ rcnb/users/management/commands/create_superuser_with_password.py
@@ -2,10 +2,12 @@
 
 import os
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Command(BaseCommand):
-    help = 'Creates a superuser from environment variables.'
+    help = 'Creates a superuser from environment variables if one does not exist.'
 
     def handle(self, *args, **options):
         username = os.environ.get('ADMIN_USER')
@@ -13,11 +15,12 @@ class Command(BaseCommand):
         password = os.environ.get('ADMIN_PASS')
 
         if not all([username, email, password]):
-            self.stdout.write(self.style.ERROR('Missing ADMIN_USER, ADMIN_EMAIL, or ADMIN_PASS environment variables.'))
+            self.stdout.write(self.style.ERROR('ADMIN_USER, ADMIN_EMAIL, or ADMIN_PASS environment variables are not set.'))
             return
 
         if User.objects.filter(username=username).exists():
-            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" already exists.'))
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" already exists. Skipping creation.'))
         else:
+            self.stdout.write(f'Creating superuser "{username}"...')
             User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser "{username}"'))
