967ec385b192ff56a2bcca3f84038199319121ff
Sternee <sternee2332006@gmail.com>
2025-09-22 22:01:53 +0530

    SUCCESS COMMIT #15

--- rcnb/cart/context_processors.py
+++ rcnb/cart/context_processors.py
@@ -1,8 +1,14 @@
 # rcnb/cart/context_processors.py
 from .models import Cart
+from asgiref.sync import sync_to_async
 
-def cart_item_count(request):
+@sync_to_async
+def get_cart_item_count(user):
+    cart, created = Cart.objects.get_or_create(user=user)
+    return cart.items.count()
+
+async def cart_item_count(request):
     if request.user.is_authenticated:
-        cart, created = Cart.objects.get_or_create(user=request.user)
-        return {'cart_item_count': cart.items.count()}
+        count = await get_cart_item_count(request.user)
+        return {'cart_item_count': count}
     return {'cart_item_count': 0}
