18a139940a67bb2d657a225c1c996964a7f6397f
Sternee <sternee2332006@gmail.com>
2025-09-22 21:18:19 +0530

    SUCCESS COMMIT #13.1

--- rcnb/rcnb/settings.py
+++ rcnb/rcnb/settings.py
@@ -157,13 +157,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # --- Email Settings ---
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = "apikey"  # This literal string, not your email
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+
 
 # --- Authentication and Authorization ---
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
