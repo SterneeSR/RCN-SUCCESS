6bc8feebdf014ba146dc5ecbf280e46a7bb8c68c
Sternee <sternee2332006@gmail.com>
2025-09-25 15:11:34 +0530

    SUCCESS COMMIT #34

--- rcnb/rcnb/wsgi.py
+++ rcnb/rcnb/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for rcnb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# rcnb/rcnb/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rcnb.settings')
+
+# --- TEMPORARY CODE TO CHECK CLOUDINARY ---
+# This will run the check_cloudinary command every time the app starts.
+# The output will be in your Railway deployment logs.
+print("--- RUNNING CLOUDINARY CHECK ---")
+try:
+    call_command('check_cloudinary')
+except Exception as e:
+    print(f"Error running check_cloudinary: {e}")
+print("--- CLOUDINARY CHECK COMPLETE ---")
+# --- REMOVE THIS CODE AFTER DEBUGGING ---
 
 application = get_wsgi_application()
