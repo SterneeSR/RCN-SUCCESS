c091326b3c156b791237b08b68d33253bb0578cd
Sternee <sternee2332006@gmail.com>
2025-09-25 12:32:00 +0530

    SUCCESS COMMIT #30.1

--- rcnb/start.sh
+++ rcnb/start.sh
@@ -3,5 +3,6 @@ set -o errexit
 
 python manage.py migrate
 python manage.py create_superuser_with_password
+python manage.py check_cloudinary.py
 
 gunicorn rcnb.wsgi:application --workers 3 --threads 2 --timeout 60 --log-file
