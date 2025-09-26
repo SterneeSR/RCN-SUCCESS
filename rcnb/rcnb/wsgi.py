87a03c9d7773441760f68a5d125957ee9df081c8
Sternee <sternee2332006@gmail.com>
2025-09-26 10:49:07 +0530

    SUCCESS COMMIT #35

--- rcnb/rcnb/wsgi.py
+++ rcnb/rcnb/wsgi.py
@@ -1,19 +1,16 @@
-# rcnb/rcnb/wsgi.py
+"""
+WSGI config for rcnb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rcnb.settings')
-
-# --- TEMPORARY CODE TO TEST COMMANDS ---
-print("--- RUNNING TEST COMMAND ---")
-try:
-    # Call the new, simple test command
-    call_command('test_command')
-except Exception as e:
-    print(f"Error running test_command: {e}")
-print("--- TEST COMMAND COMPLETE ---")
-# --- REMOVE THIS CODE AFTER DEBUGGING ---
 
 application = get_wsgi_application()
