bbfd302410cde732aacb7cdb07a79a793d8d2cc6
Sternee <sternee2332006@gmail.com>
2025-09-27 11:31:22 +0530

    SUCCESS COMMIT #51

--- rcnb/products/urls.py
+++ rcnb/products/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
+    path("check-storage/", views.check_storage),
     path("test-cloudinary/", views.test_cloudinary_upload),
     path('', views.product_list, name='product_list'),
     path('suggest/', views.product_suggest, name='product_suggest'),
