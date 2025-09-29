68ce1c756f60c37cb57e4dfae9b771fb7aedf3e6
Sternee <sternee2332006@gmail.com>
2025-09-29 19:31:57 +0530

    Events Commit #6

--- rcnb/events/models.py
+++ rcnb/events/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     short_description = models.TextField()
     full_description = models.TextField()
-    image = models.ImageField(upload_to='events/')
+    image = models.ImageField(upload_to='events/', blank=True, null=True)
     linkedin_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
@@ -13,10 +14,10 @@ class Event(models.Model):
 
 class UpcomingEvent(models.Model):
     title = models.CharField(max_length=200)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     short_description = models.TextField()
     full_description = models.TextField()
-    image = models.ImageField(upload_to='upcoming_events/')
+    image = models.ImageField(upload_to='upcoming_events/', blank=True, null=True)
     linkedin_url = models.URLField(blank=True, null=True)
     posted_on = models.DateField(auto_now_add=True)
 
@@ -25,10 +26,10 @@ class UpcomingEvent(models.Model):
 
 class Update(models.Model):
     title = models.CharField(max_length=200)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     short_description = models.TextField()
     full_description = models.TextField()
-    image = models.ImageField(upload_to='updates/')
+    image = models.ImageField(upload_to='updates/', blank=True, null=True)
     linkedin_url = models.URLField(blank=True, null=True)
     posted_on = models.DateField(auto_now_add=True)
 
