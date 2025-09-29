a3dbb4629a4a1845766a2df6d766456de648dac3
Sternee <sternee2332006@gmail.com>
2025-09-29 11:11:01 +0530

    Added Domain

--- rcnb/rcnb/settings.py
+++ rcnb/rcnb/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOSTS:
     ALLOWED_HOSTS = ALLOWED_HOSTS.split(',')
 else:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'raise-nanobiotech.up.railway.app', 'rcn-success.onrender.com']
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'raise-nanobiotech.up.railway.app', 'rcn-success.onrender.com', 'rcn-web.up.railway.app']
 
 
 CSRF_TRUSTED_ORIGINS = [
