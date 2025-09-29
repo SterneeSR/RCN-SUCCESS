ab22a253c736960bc040ced12dc300a7bdf83a01
Sternee <sternee2332006@gmail.com>
2025-09-29 19:18:35 +0530

    Events Commit #4

--- rcnb/build.sh
+++ rcnb/build.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Create and apply database migrations for ALL apps
+python manage.py makemigrations
+python manage.py migrate
