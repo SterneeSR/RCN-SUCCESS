231d2bd92b45b1368793151a7237a099ecb10472
Sternee <sternee2332006@gmail.com>
2025-09-30 14:24:30 +0530

    Order URL fixing

--- rcnb/orders/urls.py
+++ rcnb/orders/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.order_list, name='order_list'),  # Orders list page
     path('checkout/', views.create_order, name='create_order'),
     path('checkout/payment/', views.payment_page, name='payment_page'),
-    path('success/<int:order_id>/', views.order_success, name='order_success'),
+    path('success/<str:order_id>/', views.order_success, name='order_success'),
 ]
